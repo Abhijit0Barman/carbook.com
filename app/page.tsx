4bbb72caad36420d8227e8f035fbc6ea0731d2ce
Abhijit0barman <abhijitbarman96@gmail.com>
2024-04-05 22:52:44 +0530

    Update title, description and add UserButton from ClerkProvider
    - Changes title and description in app/layout.tsx
    - Adds UserButton component from ClerkProvider in app/page.tsx
    - Updates middleware.ts to include file extensions in regex ([w]+ to [\\w]+)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>CarBook.com</h2>
+      <div className="h-screen">
+        <UserButton />
+      </div>
     </div>
   );
 }
